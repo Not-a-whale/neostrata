eb6c87947cb4d04045d382e7ca26903c263e43f3
Mauro <maurogreca@gmail.com>
2018-09-28 10:30:41 -0300

    Adding navigation for swiper

--- scripts/modules/product/product-carousel-swiper.js
+++ scripts/modules/product/product-carousel-swiper.js
@@ -5,10 +5,10 @@
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
         spaceBetween: 30,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
         breakpoints: {
             1024: {
                 slidesPerView: 3
