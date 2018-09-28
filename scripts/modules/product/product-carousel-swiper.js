6577a0b1c4ff88ec04a581f88c1bcc909778896d
Mauro <maurogreca@gmail.com>
2018-09-27 23:11:32 -0300

    Carousel implemented with swiper

---
+++ scripts/modules/product/product-carousel-swiper.js
@@ -0,0 +1,30 @@
+﻿define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', 'swiper'], function($, Swiper) {
+    $(document).ready(function() {
+       console.log('product-carousel-swiper ready');
+    });
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 3
+            },
+            992: {
+                slidesPerView: 3
+            },
+            768: {
+                slidesPerView: '1'
+            },
+            640: {
+                slidesPerView: '1'
+            },
+            320: {
+                slidesPerView: '1'
+            }
+        }
+    });
+});
