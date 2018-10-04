9edc0ee3e121943b31d4e22d3f3d8beb0e63e702
Mauro <maurogreca@gmail.com>
2018-10-04 15:28:42 -0300

    Fix styles in header, facets and directory page

--- scripts/pages/category.js
+++ scripts/pages/category.js
@@ -1,9 +1,7 @@
 define([
     'modules/jquery-mozu',
-    "modules/views-collections",
-    "elevatezoom"], function($, CollectionViewFactory, elevatezoom) {
+    "modules/views-collections"], function($, CollectionViewFactory) {
     $(document).ready(function() {
-        $('.mz-productlisting-image img').elevateZoom({ zoomType: "inner", cursor: "crosshair" });
         window.facetingViews = CollectionViewFactory.createFacetedCollectionViews({
             $body: $('[data-mz-category]'),
             template: "category-interior"
