335c64847ce9b0231c33f37a87c3f61a117f8865
Mauro <maurogreca@gmail.com>
2018-10-11 15:11:13 -0300

    Fix price alignment

--- scripts/modules/product/product-carousel-swiper.js
+++ scripts/modules/product/product-carousel-swiper.js
@@ -26,6 +26,7 @@
         }
     });
     function window_resize(){
+        $('.product-listing-container').css('height', '');
         var maxHeight = Math.max.apply(null, $(".product-listing-container").map(function ()
         {
             return $(this).height() + parseFloat($(this).css("padding-top")) + parseFloat($(this).css("padding-bottom"));
