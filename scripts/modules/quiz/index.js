68c1b598909ad1b9120439f38d538975c78e10bd
Zebulon Young <zebulonj@gmail.com>
2019-01-06 21:53:26 -0800

    Stub quiz widget.

---
+++ scripts/modules/quiz/index.js
@@ -0,0 +1,16 @@
+/* global $BV */
+define([
+  'modules/jquery-mozu',
+  'underscore',
+  'modules/backbone-mozu',
+  'modules/api',
+  'hyprlive'
+], function(
+  $,
+  _,
+  Backbone,
+  API,
+  Hypr
+) {
+
+});
