8ee98a16044d73835bb62e3e13a52bde9b3d1bff
Valerii Kot <kotvalera@gmail.com>
2019-01-09 20:01:53 +0200

    Before and after slider style

---
+++ scripts/modules/product/product-carousel-swiper-b-and-f.js
@@ -0,0 +1,48 @@
+﻿define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', 'swiper'], function($, Swiper) {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        loop: false,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 2
+            },
+            992: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: '2'
+            },
+            640: {
+                slidesPerView: '2'
+            },
+            320: {
+                slidesPerView: '2'
+            }
+        }
+    });
+    function window_resize(){
+        $('.product-listing-container').css('height', '');
+        var maxHeight = Math.max.apply(null, $(".product-listing-container").map(function ()
+        {
+            return $(this).height() + parseFloat($(this).css("padding-top")) + parseFloat($(this).css("padding-bottom"));
+        }).get());
+        $('.product-listing-container').css('height', maxHeight);
+    }
+    $(document).ready(function() {
+        window_resize();
+    });
+    $(window).resize(function() {
+        window_resize();
+    });
+
+
+});
