549f405b6cffebddd336e7749772708f1e8870cc
Eduardo De Lisa <eduardo@deplabs.com>
2019-01-19 10:29:27 -0300

    fix saved address on checkout

--- scripts/pages/checkout.js
+++ scripts/pages/checkout.js
@@ -109,7 +109,7 @@ require(["modules/jquery-mozu",
         renderOnChange: [
             'address.countryCode',
             'contactId'
-        ],
+    ],
         beginAddContact: function () {
             this.model.set('contactId', 'new');
         },
@@ -118,6 +118,10 @@ require(["modules/jquery-mozu",
         },
         allowDigit:function(e){
             e.target.value= e.target.value.replace(/[^\d]/g,'');
+        },
+        render: function() {
+            CheckoutStepView.prototype.render.apply(this );
+            $('.selectpicker').selectpicker();
         }
     });
 
