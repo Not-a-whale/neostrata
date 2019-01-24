c74a3d05c3c62cde336260684def4f9561847e55
Valerii Kot <kotvalera@gmail.com>
2019-01-24 22:29:56 +0200

    convert to widget

---
+++ scripts/widgets/featured-products.js
@@ -0,0 +1,21 @@
+﻿define(['modules/jquery-mozu', "modules/api"],
+    function ($, api) {
+        $(document).ready(function() {
+            window.productRegimenCollection = [];
+            var totalPrice = 0;
+            var count = 0;
+            var cat = $('.regimenProducts').data('mzCategoryRegimen');
+            if(cat){
+                api.get("search", { filter: "categoryId eq " + cat }).then(function(productResponse){
+                    count = productResponse.length;
+                    for(var i=0; productResponse.length; i++){
+                        window.productRegimenCollection.push(productResponse[i].data.productCode);
+                        totalPrice = totalPrice + productResponse[i].data.price.price;
+                    }
+                });
+                setTimeout(function() {
+                    $('.countProd').html( count + ' PRODUCTS - $' + totalPrice.toFixed(2));
+                }, 1000);
+            }
+        });
+    });
