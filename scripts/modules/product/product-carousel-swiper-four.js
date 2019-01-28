101ecac66efb8d64e0588ff8dc350aa3321d12f9
AlexB <badzym@gmail.com>
2019-01-28 18:56:28 +0200

    Phisician Page Fixes - after deployment to develop need to move changes from sandbox to prod env

---
+++ scripts/modules/product/product-carousel-swiper-four.js
@@ -0,0 +1,28 @@
+﻿define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', 'swiper'], function($, Swiper) {
+    var swiper = new Swiper('.swiper-container-4', {
+        slidesPerView: 4,
+        spaceBetween: 0,
+        loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+        breakpoints: {
+            1024: {
+                slidesPerView: 4
+            },
+            992: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: '1'
+            },
+            640: {
+                slidesPerView: '1'
+            },
+            320: {
+                slidesPerView: '1'
+            }
+        }
+    });
+});
