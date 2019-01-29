3b3a1629d56531afb7f6dba9f47b5e281537da09
Valerii Kot <kotvalera@gmail.com>
2019-01-29 20:22:22 +0200

    featured product only

---
+++ scripts/widgets/featured-product-only/slider.js
@@ -0,0 +1,11 @@
+﻿define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', 'swiper'], function($, Swiper) {
+    var swiper = new Swiper('.mz-productimages-thumbs > div', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		}
+    });
+});
