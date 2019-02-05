acded026a481b1a5a9bc3ed14ab60b7a63838fb8
Milagros Schneider <milagros@deplabs.com>
2019-02-05 12:47:53 -0300

    DEPLabs: map to product image and product URL

--- scripts/pages/order-status-detail.js
+++ scripts/pages/order-status-detail.js
@@ -9,9 +9,41 @@ require([
     "async",
     'modules/my-account-order-status',
     'modules/models-order-status',
+    'modules/models-product',
     'modules/editable-view'
-], function($, _, blockUiLoader, Hypr, Backbone, HyprLiveContext, api, async, OrderStatusApi, OrderStatusModels, EditableView) {
+], function($, _, blockUiLoader, Hypr, Backbone, HyprLiveContext, api, async, OrderStatusApi, OrderStatusModels, ProductModels,  EditableView) {
 
+    var getMozuProducts = function(orderProductList) {
+            var deferred = api.defer();
+            var numReqs = orderProductList.length;
+            var productList = [];
+            var filter = "";
+            _.each(orderProductList, function(attrs) {
+                if (filter !== "") filter += " or ";
+                filter += "productCode eq "+ attrs;    
+            });
+            var op = api.get('products', filter);                
+            op.then(function(data) {
+                console.log('OrderStatusDetail::getMozuProducts() --> in then result data: ', data); 
+                if (data.data.items && data.data.items.length === 0) {
+                    _.defer(function() {
+                        deferred.resolve(productList);
+                    });
+                }
+                _.each(data.data.items, function(product){
+                    productList.push(product);
+                    _.defer(function() {
+                        deferred.resolve(productList);
+                    });
+                });
+
+            }, function(reason){
+                _.defer(function() {
+                    deferred.resolve(productList);
+                });                    
+            });
+        return deferred.promise;
+    };
 
     var OrderStatusView =  EditableView.extend({ //Backbone.MozuView.extend({
     templateName: "modules/order/order-status-detail",
@@ -90,18 +122,56 @@ require([
             console.log('Data in then ', data); 
             if( data  ){
                 var orderStatusModel = new OrderStatusModels.OmxOrderStatus(data);
-                console.log('OmxOrderStatus ', orderStatusModel);
-                var omxOrderStatusView = new OrderStatusView({
-                    el: $( '#order-status-detail' ),
-                    model: orderStatusModel,
-                    messagesEl: $('[data-mz-message-bar]')
+
+                var productIds = []; 
+
+                data.orderDetail.OrderDetail[0].LineItem.forEach( function(lineItem) {
+                    productIds.push(lineItem.FulfillmentReference[0]); 
+                }); 
+                
+                getMozuProducts(productIds).then(function (products) {
+                    var prodColl; 
+                    if (products.length !== 0) {
+                        prodColl = new ProductModels.ProductCollection();
+                        prodColl.set('items', products);
+                    } 
+
+                    orderStatusModel.attributes.orderDetail.ProductCollection= prodColl; 
+                    orderStatusModel.attributes.orderDetail.OrderDetail[0].LineItem.forEach( function(lineItem) {
+                        var apiProduct = ''; 
+                        if (lineItem.ItemCode[0]._ != 'MLCOUPON') {
+                            prodColl.attributes.items.models.forEach( function(pInApi) {
+                                if (lineItem.ItemCode[0]._ === pInApi.attributes.productCode ){
+                                    apiProduct = pInApi; 
+                                }
+                            });
+                            if (apiProduct) {
+                                lineItem.url = apiProduct.attributes.url; 
+                                lineItem.image = apiProduct.attributes.content.attributes.productImages[0].imageUrl; 
+                            }
+                        }
+                    }); 
+
+                    var omxOrderStatusView = new OrderStatusView({
+                        el: $( '#order-status-detail' ),
+                        model: orderStatusModel,
+                        messagesEl: $('[data-mz-message-bar]')
+                    });
+    
+                    //self.render();
+                    window.orderStatusView = omxOrderStatusView;
+                    omxOrderStatusView.render();
+                    blockUiLoader.unblockUi();
+/*                productIds.forEach(function(prodCode){
+                    api.get('product', { productCode: prodCode }).then(function(product) {
+                            console.log(product.prop('productCode')); // -> 'EXAMPLE123'
+                            console.log(product.data.productCode); // -> 'EXAMPLE123'
+    
+                    }).catch(function(error) {
+                        console.log('issue in the apiCall to Product', error);
+                    }); */
+
                 });
-
-                //self.render();
-                window.orderStatusView = omxOrderStatusView;
-
-                omxOrderStatusView.render();
-                blockUiLoader.unblockUi();
             }
         },function(error){
         console.log('error during the api call', error);
