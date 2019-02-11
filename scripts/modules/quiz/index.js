9be21fbed015bab8b9459e2b2392af39b005e2c3
Huw Roberts <huw@deplabs.com>
2019-02-11 15:00:52 -0800

    #34279 - add reg marks

--- scripts/modules/quiz/index.js
+++ scripts/modules/quiz/index.js
@@ -43,7 +43,7 @@ define([
     {
       value: 'aging',
       label: 'AGING',
-      description: 'Due mostly to cumulative sun exposure, visible signs of aging skin include wrinkles, lax or sagging skin, uneven pigmentation and rough texture. Aminofil, NeoGlucosamine & Retinol are key ingredients in treating the look of aging skin.',
+      description: 'Due mostly to cumulative sun exposure, visible signs of aging skin include wrinkles, lax or sagging skin, uneven pigmentation and rough texture. Aminofil®, NeoGlucosamine® & Retinol are key ingredients in treating the look of aging skin.',
       answer: 'I want to improve aging skin.'
     },
     {
@@ -73,19 +73,19 @@ define([
     {
       value: 'dark-circles',
       label: 'DARK CIRCLES & CROW’S FEET',
-      description: 'Dark circles and the little lines around your eyes (crow’s feet) become more prominent as skin thins with age. Gentle products formulated with Gluconolactone (PHA), Lactobionic Acid (PHAs)and NeoGlucosamine™ can help with this common issue in the delicate eye area.',
+      description: 'Dark circles and the little lines around your eyes (crow’s feet) become more prominent as skin thins with age. Gentle products formulated with Gluconolactone (PHA), Lactobionic Acid (PHAs)and NeoGlucosamine® can help with this common issue in the delicate eye area.',
       answer: 'I want to improve dark circles and crows feet.'
     },
     {
       value: 'spots',
       label: 'UNEVEN SKIN TONE & DARK SPOTS',
-      description: 'Uneven skin tone and dark spots appear as areas of  skin discoloration on the face, chest, shoulders or hands. Also known as sun spots, age spots and dark spots. Look for products that contain Gluconolactone (PHA), NeoGlucosamine and Vitamin C.',
+      description: 'Uneven skin tone and dark spots appear as areas of  skin discoloration on the face, chest, shoulders or hands. Also known as sun spots, age spots and dark spots. Look for products that contain Gluconolactone (PHA), NeoGlucosamine® and Vitamin C.',
       answer: 'I want to improve uneven skin tone and dark spots.'
     },
     {
       value: 'sagging',
       label: 'LAX OR SAGGING SKIN',
-      description: 'Lax or Sagging Skin (laxity) is due to accumulated sun exposure and gravity. Ingredients like Aminofil™, NeoGlucosamine, and NeoCitriate target and treat lax and sagging skin for improved tone and firmness.',
+      description: 'Lax or Sagging Skin (laxity) is due to accumulated sun exposure and gravity. Ingredients like Aminofil®, NeoGlucosamine®, and NeoCitriate® target and treat lax and sagging skin for improved tone and firmness.',
       answer: 'I want to improve lax or sagging skin.'
     },
     {
@@ -97,7 +97,7 @@ define([
     {
       value: 'dull',
       label: 'DULL SKIN ',
-      description: 'The cumulative effects of sun exposure and photoaging can make skin look dull and lacking in natural glow. Products that contain Glycolic Acid (AHA), Vitamin C and NeoGlucosamine™ and Retinol work to exfoliate and brighten skin.',
+      description: 'The cumulative effects of sun exposure and photoaging can make skin look dull and lacking in natural glow. Products that contain Glycolic Acid (AHA), Vitamin C and NeoGlucosamine® and Retinol work to exfoliate and brighten skin.',
       answer: 'I want to improve dull skin.'
     }
   ];
@@ -318,7 +318,7 @@ define([
     'CLARIFY': {
       slug: 'clarify',
       name: 'CLARIFY',
-      description: 'A targeted collection of products for oily and acne-prone skin. Formulated to clear clogged pores and reduce oil and blemishes. The exfoliating power of Glycolic and Mandelic Acids and NeoGlucosamine™, ' +
+      description: 'A targeted collection of products for oily and acne-prone skin. Formulated to clear clogged pores and reduce oil and blemishes. The exfoliating power of Glycolic and Mandelic Acids and NeoGlucosamine®, ' +
         'in this collection helps refine surface texture for smoother, healthier-looking skin.',
       products: {
         cleanser: null,
@@ -340,7 +340,7 @@ define([
     'ENLIGHTEN': {
       slug: 'enlighten',
       name: 'ENLIGHTEN',
-      description: 'A unique collection of products that addresses a multitude of clarity and discoloration issues. Powerful skin brighteners such as Vitamin C, Retinol and NeoGlucosamine help reveal even-toned, glowing skin.',
+      description: 'A unique collection of products that addresses a multitude of clarity and discoloration issues. Powerful skin brighteners such as Vitamin C, Retinol and NeoGlucosamine® help reveal even-toned, glowing skin.',
       products: {
         cleanser: null,
         toner: null,
@@ -438,12 +438,12 @@ define([
       'it ideal for dry skin.',
 
     'AMINOFIL':
-      'Aminofil™ is a revolutionary, patented amino acid derivative. It targets expression lines ' +
-      'such as scowl lines, furrows, smile lines and crow’s feet. Aminofil™ is the ingredient ' +
+      'Aminofil® is a revolutionary, patented amino acid derivative. It targets expression lines ' +
+      'such as scowl lines, furrows, smile lines and crow’s feet. Aminofil® is the ingredient ' +
       'for volume building, plumping and firming the look of skin.',
 
     'NEOGLUCOSAMINE':
-      'NeoGlucosamine™ is a patented, non-acid amino sugar that improves discoloration and ' +
+      'NeoGlucosamine® is a patented, non-acid amino sugar that improves discoloration and ' +
       'diminishes the appearance of fine lines & wrinkles. Gently exfoliates to reveal fresh ' +
       'new cells. It’s also a building block of skin’s natural Hyaluronic Acid and helps ' +
       'promote firm skin.',
