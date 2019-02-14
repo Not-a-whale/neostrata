5ee51b091c7561ef145397425b491a1a33bfc68f
Huw Roberts <huw@deplabs.com>
2019-02-14 09:51:03 -0800

    Don't use factory method for creating View

--- scripts/modules/free-samples/free-samples.js
+++ scripts/modules/free-samples/free-samples.js
@@ -56,57 +56,19 @@ define([
     });
 
     var FreeSamplesView = Backbone.MozuView.extend({
-        templateName: 'modules/product/free-sample-widget/product-list-tiled'
-    });
-
-    function factory(conf) {
-
-        var _$body = conf.$body;
-        var _dispatcher = UrlDispatcher;
-
-        var categoryId = conf.$body.data('mz-free-samples');
-
-
-        /*directory Add-To-Cart action */
-        var directoryAddToCart = IntentEmitter(_$body,
-                                               ['click #product-list-ul .mz-productdetail-addtocart',
-                                                'click #more-list-ul .mz-productdetail-addtocart'],
-                                               directoryAddToCartAction);
-
-        function directoryAddToCartAction(_e){
-            var productCode = $(_e.currentTarget).data("mz-product-code");
-
-            var itemsInCart = GlobalCart.getItemsCache();
-
-            if(productCode && productCode !== ''){
-
-                api.get('product', productCode).then(function(productResponse){
-                    var product = new ProductModels.Product(productResponse.data);
-                    product.on('addedtocart', function(cartitem) {
-                        if (cartitem && cartitem.prop('id')) {
-                            //product.isLoading(true);
-                            CartMonitor.addToCount( product.get('quantity'), true);
-                            MetricsEngine.trackDirectoryAddToCart(product, product.get('categories')[0], false, 1);
-                        }
-                        else {
-                            product.trigger("error", { message: Hypr.getLabel('unexpectedError') });
-                        }
-                        return location.reload();
-                    });
-                    product.addToCart();
-                });
-            }
-        }
-
-        $(document).ready(function() {
-          var cart = CartModels.Cart.fromCurrent();
+        templateName: 'modules/product/free-sample-widget/product-list-tiled',
+        /*events: {
+          'click #product-list-ul .mz-productdetail-addtocart': 'addToCart'
+        },*/
+        initialize: function(conf) {
+          var me = this;
+          console.log('Init called', conf);
           var freeSamplesCollection = new FreeSamplesCollection();
-          //console.log('Cart is', cart);
-          var cartTotal = cart.attributes.subtotal;
-          //console.log('Subtotal is' +  cartTotal);
+          this.model = new FreeSamplesModel( {
+            freeSamples: freeSamplesCollection
+          });
 
-
-          api.get('products', "categoryId req " + categoryId).then( function( categoryResponse ) {
+          api.get('products', "categoryId req " + conf.categoryId).then( function( categoryResponse ) {
             categoryResponse.data.items.forEach( function(product) {
               var newItem = new FreeSampleItem( {
                 productCode: product.productCode,
@@ -114,58 +76,98 @@ define([
               });
               freeSamplesCollection.add(newItem);
             });
-
-            var freeSamplesCount = 0;
-            var cartItems = cart.get('items');
-            cartItems.forEach( function(item) {
-              var cartProductCode = item.get('product').get('productCode');
-              // find this product in free samples and set it
-              var sample = freeSamplesCollection.get(cartProductCode);
-              if (sample) {
-                sample.set('inCart', true);
-                freeSamplesCount++;
-              }
-            });
-
-            try {
-              var theModel;
-              if (cartTotal < FREE_SAMPLE_THRESHOLD) {
-                theModel = new FreeSamplesModel( {
-                  freeSamples: null,
-                  message: "Add $" + (FREE_SAMPLE_THRESHOLD - cartTotal) + " more for free shipping and " + FREE_SAMPLE_MAX + " free samples!",
-                  freeSamplesCount: freeSamplesCount
-                });
-              }
-              else if (freeSamplesCount < 3) {
-                theModel = new FreeSamplesModel( {
-                  freeSamples: freeSamplesCollection,
-                  message: "You’ve earned 3 FREE SAMPLES. Choose them below.",
-                  freeSamplesCount: freeSamplesCount
-                });
-              }
-              else {
-                theModel = new FreeSamplesModel( {
-                  freeSamples: null,
-                  message: "You have chosen your free samples!",
-                  freeSamplesCount: freeSamplesCount
-                });
-              }
-              var theView = new FreeSamplesView({
-                  el: _$body, //$('[mz-free-samples]'),
-                  model: theModel
-              });
-              theView.render();
-            }
-            catch (e) {
-              console.log(e);
+            console.log('calling update');
+            me.update();
+          });
+        },
+        update: function() {
+          console.log('Update called');
+          var me = this;
+          var cart = CartModels.Cart.fromCurrent();
+          //console.log('Cart is', cart);
+          var cartTotal = cart.attributes.subtotal;
+          //console.log('Subtotal is' +  cartTotal);
+          var freeSamplesCollection = me.model.get('freeSamples');
+          var freeSamplesCount = 0;
+          var cartItems = cart.get('items');
+          cartItems.forEach( function(item) {
+            var cartProductCode = item.get('product').get('productCode');
+            // find this product in free samples and set it
+            var sample = freeSamplesCollection.get(cartProductCode);
+            if (sample) {
+              sample.set('inCart', true);
+              freeSamplesCount++;
             }
           });
-        });
-    }
 
-    return {
+          var setModel = function( message, count) {
+            me.model.set('message', message);
+            me.model.set('freeSamplesCount', count);
+          };
 
-        createFreeSamplesViews: factory
-    };
+          try {
+            if (cartTotal < FREE_SAMPLE_THRESHOLD) {
+              setModel(
+                "Add $" + (FREE_SAMPLE_THRESHOLD - cartTotal) + " more for free shipping and " + FREE_SAMPLE_MAX + " free samples!",
+                freeSamplesCount
+              );
+            }
+            else if (freeSamplesCount < 3) {
+              setModel(
+                "You’ve earned 3 FREE SAMPLES. Choose them below.",
+                freeSamplesCount
+              );
+            }
+            else {
+              setModel(
+                "You have chosen your free samples!",
+                freeSamplesCount
+              );
+            }
+            me.render();
+          }
+          catch (e) {
+            console.log(e);
+          }
+        },
+        /*render: function() {
+          console.log('render called');
+        },*/
+        addSampleToCart: function(_e, a, b, c) {
+          var me = this;
+          console.log('Add to cart');
+          console.log('e', _e);
+          console.log('a', a);
+          console.log('b', b);
+          console.log('c', c);
+
+          var productCode = $(_e.currentTarget).data("mz-product-code");
+
+          var itemsInCart = GlobalCart.getItemsCache();
+
+          if(productCode && productCode !== ''){
+
+              api.get('product', productCode).then(function(productResponse){
+                  var product = new ProductModels.Product(productResponse.data);
+                  product.on('addedtocart', function(cartitem) {
+                      if (cartitem && cartitem.prop('id')) {
+                          console.log('Calling update', me);
+                          me.update();
+                          CartMonitor.addToCount( product.get('quantity'), true);
+                          MetricsEngine.trackDirectoryAddToCart(product, product.get('categories')[0], false, 1);
+                      }
+                      else {
+                          product.trigger("error", { message: Hypr.getLabel('unexpectedError') });
+                      }
+
+                  });
+                  product.addToCart();
+              });
+          }
+        }
+
+    });
+
+    return FreeSamplesView;
 
 });
