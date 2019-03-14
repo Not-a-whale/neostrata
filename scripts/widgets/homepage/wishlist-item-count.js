388e73cf3341d0e4478a37ae57c2355af5b91d07
Valerii Kot <kotvalera@gmail.com>
2019-03-14 22:44:08 +0200

    33980 Wishlist Item Count

---
+++ scripts/widgets/homepage/wishlist-item-count.js
@@ -0,0 +1,15 @@
+require([
+    "modules/jquery-mozu",
+    "modules/api"
+], function($, api) {
+    var user = require.mozuData('user');
+    $(document).ready(function(){
+      if(user.accountId){
+          api.createSync('wishlist').getOrCreate(user.accountId).then(function(wishlist) {
+              return wishlist.data;
+          }).then(function(wishlistItems) {
+              $('.wishlist-item-count > .heart > .count-number').html(wishlistItems.items.length + '<br/>Items');
+          });
+      }
+    });
+});
