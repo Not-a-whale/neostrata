31fc2a5dc1dce685374b78c21a16ea0f0e0f75c8
Valerii Kot <kotvalera@gmail.com>
2019-03-15 22:06:04 +0200

    33515 desktop view

--- scripts/widgets/featured-product-only/slider.js
+++ scripts/widgets/featured-product-only/slider.js
@@ -1,11 +1,15 @@
-﻿define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', 'swiper'], function($, Swiper) {
-    var swiper = new Swiper('.mz-productimages-thumbs > div', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev'
-		}
-    });
+﻿define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', "bxslider"], function($, bxslider) {
+      var slider = $('#productpager-Carousel').bxSlider({
+          slideWidth: 125,
+          minSlides: 3,
+          maxSlides: 3,
+          moveSlides: 1,
+          slideMargin: 15,
+          nextText: '<i class="fa fa-caret-right" aria-hidden="true"></i>',
+          prevText: '<i class="fa fa-caret-left" aria-hidden="true"></i>',
+          infiniteLoop: false,
+          hideControlOnEnd: true,
+          pager: false
+      });
+      window.slider = slider;
 });
